3bbc1def5d6725fa859c331a87ffae24daaa1c1f
KI-Anik <iamkhairul101@gmail.com>
2024-12-10 14:51:57 +0600

    dashborad card on display

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Heading from '../components/Heading';
+import { getAllFav } from '../components/Utils';
+import Card from '../components/Card';
 
 const Dashboard = () => {
+    const [coffees, setCoffees] = useState([])
+    useEffect(() => {
+        const favourite = getAllFav()
+        setCoffees(favourite)
+    }, [])
+
     return (
         <div>
-            Dashboard
+            <Heading
+                title='Welcome to Dashboard'
+                subTitle=' Manage coffees that you have previously added as favorite. You can view or remove them from here'
+            ></Heading>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 gap-6'>
+                {
+                    coffees.map((coffee) => <Card key={coffee.id} coffee={coffee} ></Card>)
+                }
+            </div>
         </div>
     );
 };
